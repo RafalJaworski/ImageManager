c85a0880fd8ad2d7119ea2b52c2c1c4110e74aa8
adminLuxPrice <admin@lux-price.pl>
2015-06-21 16:32:08 +0200

    Configure the ServiceManager so that it knows how to get a Zend\Db\Adapter\Adapter

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,24 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+         'driver'         => 'Pdo',
+         'dsn'            => 'mysql:dbname=image_manager;host=localhost',
+         'driver_options' => array(
+             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         ),
+     ),
+     'service_manager' => array(
+        'factories' => array(
+        'Zend\Db\Adapter\Adapter'  //This kye is used in Module.php -> $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+        => 'Zend\Db\Adapter\AdapterServiceFactory',
+    ),
+    // PUT BELOW TO THE LOCAL.PHP THAT CAN'T BE SENT TO GITHUB
+//         return array(
+//             'db' => array(
+//                 'username' => 'root',
+//                 'password' => 'root',
+//             ),
+//         );
+),
 );
